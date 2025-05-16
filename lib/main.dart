565ac493e0b5539f776eba70389fe63bdd13b3ed
Ramona Alcantara <mariaramona.alcantara@gmail.com>
2025-05-16 12:07:14 -0400

    init project

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  late TextEditingController _loginController; //this is to read what was typed
+  late TextEditingController _passwordController;
+  String imageSource = 'images/question-mark.png';
+
+
+  @override
+  void initState() {
+    super.initState();
+    _loginController = TextEditingController(); //making _controller
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _loginController.dispose();
+    _passwordController.dispose();
+    super.dispose(); // free the memory of what was typed
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+
+            TextField(
+                controller: _loginController,
+                decoration: InputDecoration(
+                    hintText:"Login",
+                    border: OutlineInputBorder(),
+                    labelText: "User login"
+                )
+            ),
+
+            TextField(
+                controller: _passwordController, obscureText: true,
+                decoration: InputDecoration(
+                    hintText:"Password",
+                    border: OutlineInputBorder(),
+                    labelText: "Password"
+                )
+            ),
+
+
+            Semantics(child: Image.asset(imageSource, width: 300, height: 300,),),
+
+            ElevatedButton( child:  Text("Login"), onPressed: () {
+              String password = _passwordController.text;
+
+              setState(() {
+                if (password == "QWERTY123") {
+                  imageSource = 'images/idea.png';
+                } else {
+                  imageSource = 'images/stop.png';
+                }
+              });
+            },),
+          ],
+        ),
+      ),
+    );
+  }
+}
