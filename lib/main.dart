b8b79557789c566b18ff7b3e440c66f385be50dd
Ramona Alcantara <mariaramona.alcantara@gmail.com>
2025-05-28 22:43:13 -0400

    lab3 in progress

--- lib/main.dart
+++ lib/main.dart
@@ -30,24 +30,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  late TextEditingController _loginController; // read what was typed as login
-  late TextEditingController _passwordController; // read what was typed as pass
-  var imageSource = 'images/question-mark.png';
-
-
-  @override
-  void initState() {
-    super.initState();
-    _loginController = TextEditingController(); //making _controller
-    _passwordController = TextEditingController();
+  // stack item generator
+  Widget generateStackItem(
+    String image,
+    String title,
+    AlignmentGeometry alignment,
+  ) {
+    return Stack(
+      //component 1
+      alignment: alignment,
+      children: [
+        CircleAvatar(backgroundImage: AssetImage(image), radius: 50),
+        Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
   }
 
-  @override
-  void dispose() {
-    _loginController.dispose();
-    _passwordController.dispose();
-    super.dispose(); // free the memory of what was typed
+  // list meat items
+  List<(String title, String image)> meats = [
+    ('BEEF', 'images/beef.jpg'),
+    ('CHICKEN', 'images/chicken.jpg'),
+    ('PORK', 'images/pork.jpg'),
+    ('SEAFOOD', 'images/seafood.jpg'),
+  ];
+
+  // generate meat items
+  List<Widget> generateRow() {
+    return meats
+        .map(
+          (data) =>
+              generateStackItem(data.$2, data.$1, AlignmentDirectional.center),
+        )
+        .toList();
+  }
+
+  // list course items
+  List<(String title, String image)> course = [
+    ('Main Dishes', 'images/maindishes.jpg'),
+    ('Salad Recipes', 'images/salad.jpg'),
+    ('Side Dishes', 'images/sidedishes.jpg'),
+    ('Crockpot', 'images/cp.jpg'),
+  ];
+
+  // generate course items
+  List<Widget> generateCourse() {
+    return course
+        .map(
+          (data) => generateStackItem(
+            data.$2,
+            data.$1,
+            AlignmentDirectional.bottomEnd,
+          ),
+        )
+        .toList();
+  }
+
+  // list dessert items
+  List<(String title, String image)> dessert = [
+    ('Ice Cream', 'images/icecream.jpg'),
+    ('Brownie', 'images/brownie.jpg'),
+    ('Pie', 'images/pie.jpg'),
+    ('Cookie', 'images/cookie.jpg'),
+  ];
+
+  // generate course items
+  List<Widget> generateDessert() {
+    return dessert
+        .map(
+          (data) =>
+              generateStackItem(data.$2, data.$1, AlignmentDirectional.center),
+        )
+        .toList();
   }
 
   @override
@@ -59,45 +119,68 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-
-
-            TextField(
-                controller: _loginController,
-                decoration: InputDecoration(
-                    hintText:"Login",
-                    border: OutlineInputBorder(),
-                    labelText: "Login"
-                )
+            // Header
+            Text(
+              "BROWSE CATEGORIES",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
 
-            TextField(
-                controller: _passwordController, obscureText: true,
-                decoration: InputDecoration(
-                    hintText:"Password",
-                    border: OutlineInputBorder(),
-                    labelText: "Password"
-                )
+            // description
+            Text(
+              "Not sure about exactly which recipe you're looking for? Do a search, or dive into our most popular categories",
+              style: TextStyle(fontSize: 15),
             ),
 
+            // MEAT SECTION
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0, top: 16.0),
+              child: Text(
+                "BY MEAT",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: generateRow(),
+            ),
 
-            Semantics(child: Image.asset(imageSource, width: 300, height: 300,),),
+            // COURSE SECTION
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0, top: 16.0),
+              child: Text(
+                "BY COURSE",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: generateCourse(),
+            ),
 
-            ElevatedButton( child:  Text("Login"), onPressed: () {
-              String password = _passwordController.text;
-
-              setState(() {
-                if (password == "QWERTY123") {
-                  imageSource = 'images/idea.png';
-                } else {
-                  imageSource = 'images/stop.png';
-                }
-              });
-            },),
+            // DESSERT SECTION
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0, top: 16.0),
+              child: Text(
+                "BY DESSERT",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: generateDessert(),
+            ),
           ],
         ),
       ),
     );
   }
+
+  // method to generate and return a list of widget
+  // List<Widget> generateMeat () {
+  //   return [
+  //    generateStackItem("images/beef.jpg", "BEEF", AlignmentDirectional.center)
+  //  ];
+  // }
 }
